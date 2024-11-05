c663b24e8e6d6d30fbd25fcbfe8740d6d66b52df
Golday24 <garrett.golday@outlook.com>
2024-11-04 18:33:22 -0600

    Applications

--- application_status/subscriptions/views.py
+++ application_status/subscriptions/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import Application, Subscription, Category
 
-# Create your views here.
+
+@login_required
+def subscription_page(request):
+    all_applications = Application.objects.all()
+    user_subscriptions = Subscription.objects.filter(user=request.user)
+    subscribed_applications = [sub.application for sub in user_subscriptions]
+
+    context = {
+        'all_applications': all_applications,
+        'subscribed_applications': subscribed_applications
+    }
+    return render(request, 'subscriptions/subscription_page.html', context)
